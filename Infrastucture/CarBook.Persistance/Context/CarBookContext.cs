f1beb384514ae470dbb9bf6ee6a42e1cded426bd
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-11-16 15:10:34 +0300

    Infrastructure layer and dbcontext class added

---
+++ Infrastucture/CarBook.Persistance/Context/CarBookContext.cs
@@ -0,0 +1,34 @@
+﻿using CarBook.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBook.Persistance.Context
+{
+    public class CarBookContext : DbContext
+    {
+
+
+        //Proje Canlıya geçerken Conf dosyasından bağlantı adresini vereceğim, development yaparken buradan alacağım bağlantı adresini.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-T54ECK5, initial Catalog=CarBookDb; integrated Security:True;");
+        }
+
+        public DbSet<About> Abouts{ get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarDescription> CarDescriptions { get; set; }
+        public DbSet<CarFeature> CarFeatures { get; set; }
+        public DbSet<CarPricing> CarPricings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<FooterAddress> FooterAddresses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Pricing> Pricingies { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
