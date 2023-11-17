8bf0b133f6b210b01c33aa9e70758ef55df647ad
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-11-17 17:10:23 +0300

    GetAboutQueryHandler added

---
+++ Core/CarBook.Application/Features/CQRS/Results/AboutResults/GetAboutQueryResult.cs
@@ -0,0 +1,12 @@
+﻿namespace CarBook.Application.Features.CQRS.Results.AboutResults
+{
+    public class GetAboutQueryResult
+    {
+        //CQRS'de queryler genellikler "get" ön ekiyle isimlendirilir.
+
+        public int AboutId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
--- Core/CarBook.Domain/obj/Debug/net8.0/CarBook.Domain.AssemblyInfo.cs
+++ Core/CarBook.Domain/obj/Debug/net8.0/CarBook.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b87908f7bb4c2bae869349e4627b12c8fd209c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
