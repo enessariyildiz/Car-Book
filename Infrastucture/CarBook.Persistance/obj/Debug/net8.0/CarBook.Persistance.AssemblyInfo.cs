88705de21464471d160f29dabc67d5c183ccba01
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-11-17 22:50:47 +0300

    GetAboutQueryHandler and AboutCommandHandler classes added

--- Infrastucture/CarBook.Persistance/obj/Debug/net8.0/CarBook.Persistance.AssemblyInfo.cs
+++ Infrastucture/CarBook.Persistance/obj/Debug/net8.0/CarBook.Persistance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Persistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bf0b133f6b210b01c33aa9e70758ef55df647ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Persistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Persistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
