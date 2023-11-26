b4d04b7cf6e8fe4039a0df2ad463794baae7db06
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-11-26 17:09:14 +0300

    Frontend aspnetcore mvc project added

--- Presentation/CarBook.WepApi/obj/Debug/net8.0/CarBook.WepApi.AssemblyInfo.cs
+++ Presentation/CarBook.WepApi/obj/Debug/net8.0/CarBook.WepApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WepApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa166b9f1a902c3524ad4fca3d0a783e7fa38c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WepApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WepApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
