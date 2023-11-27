0f904f130d785d60a58a999f9c388f5147daaa6d
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-11-27 15:26:12 +0300

    About consume added

--- Frontedns/CarBook.WebUI/ViewComponents/AboutUsComponents/_AboutUsComponentPartial.cs
+++ Frontedns/CarBook.WebUI/ViewComponents/AboutUsComponents/_AboutUsComponentPartial.cs
@@ -1,11 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.AboutUsComponents
 {
     public class _AboutUsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        // API Consume.
+        // What is Consume ? Show data from API.
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // Create client.
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7194/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+            }
+
+
+
+
+
+
             return View();
         }
     }
