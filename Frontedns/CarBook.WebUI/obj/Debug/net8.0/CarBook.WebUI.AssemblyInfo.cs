e40c2589661b142d5090b4e3e249126246f72c4b
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-11-27 15:11:16 +0300

    About page partial added

--- Frontedns/CarBook.WebUI/obj/Debug/net8.0/CarBook.WebUI.AssemblyInfo.cs
+++ Frontedns/CarBook.WebUI/obj/Debug/net8.0/CarBook.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a7b5551af60dd7dd9191e846b613794a889077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea7f7557b91e005b403388ce48a0e3809e4cb90")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
