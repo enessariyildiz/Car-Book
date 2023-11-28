7070d5aa19267ce45a43eff5f8805f32a6bc33a5
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-11-28 13:37:47 +0300

    Car list added

--- Frontedns/CarBook.WebUI/obj/Debug/net8.0/CarBook.WebUI.AssemblyInfo.cs
+++ Frontedns/CarBook.WebUI/obj/Debug/net8.0/CarBook.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ff5278c627cfcb73d2f3c42cdd971357b84bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06541545491783d65a00bb321742726788152000")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Frontends/CarBook.Dto/obj/Debug/net8.0/CarBook.Dto.AssemblyInfo.cs
+++ Frontends/CarBook.Dto/obj/Debug/net8.0/CarBook.Dto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ff5278c627cfcb73d2f3c42cdd971357b84bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06541545491783d65a00bb321742726788152000")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
