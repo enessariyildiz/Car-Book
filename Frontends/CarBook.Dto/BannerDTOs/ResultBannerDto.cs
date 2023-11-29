f0519afd80d56903d1033e947038733943a54e57
Enes Sarıyıldız <ens.sryldz@gmail.com>
2023-11-29 18:16:36 +0300

    Home page, login and banners areas added

---
+++ Frontends/CarBook.Dto/BannerDTOs/ResultBannerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.Dto.BannerDTOs
+{
+    public class ResultBannerDto
+    {
+        public int BannerId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string VideoDescription { get; set; }
+        public string VideUrl { get; set; }
+    }
+}
--- Frontends/CarBook.Dto/obj/Debug/net8.0/CarBook.Dto.AssemblyInfo.cs
+++ Frontends/CarBook.Dto/obj/Debug/net8.0/CarBook.Dto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7423dc8b6b50a2021dfc83c2b5b0de0a5a7cef43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b8056cbc64650e93c55858c70c7324e282e10f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
